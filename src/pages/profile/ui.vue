0a37769704f50cab626a0ff59916611f4b5fec5a
Daria Korchunova <bg.korchunova@gmail.com>
2024-06-04 16:33:39 +0300

    добавлены карточки желаний

--- src/pages/profile/ui.vue
+++ src/pages/profile/ui.vue
@@ -11,42 +11,46 @@ const lists = ref(listData)
 
 const modalCreate = ref(false)
 
+const isloading = ref(true);
+
 ///////
 
 // modal
 
-const showModal = ref(false)
+const showModal = ref(false);
 
-const newTitle = ref('')
-const newDescription = ref('')
-const newImageUrl = ref('')
+const newTitle = ref('');
+const newDescription = ref('');
+const newImageUrl = ref('');
 
-const notes = ref([])
+const notes = ref([]);
 
-const errorMessage = ref('')
+// const errorMessage = ref('')
 
 // function getRandomColor() {
-//   return 'hsl(' + Math.random() * 1000 + ', 100%, 70%)'
+//   return 'hsl(' + Math.random() * 1000 + ', 100%, 70%)';
 // }
 
 const addNote = () => {
-  if (newTitle.value.length < 10) {
-    return (errorMessage.value = 'note need to be 10 characters or more')
-  }
-  if (newDescription.value.length < 10) {
-    return (errorMessage.value = 'note need to be 10 characters or more')
-  }
+  // if (newTitle.value.length < 10) {
+  //   return (errorMessage.value = 'note need to be 10 characters or more')
+  // }
+  // if (newDescription.value.length < 10) {
+  //   return (errorMessage.value = 'note need to be 10 characters or more')
+  // }
   notes.value.push({
     id: Math.floor(Math.random() * 1000000),
     title: newTitle.value,
     description: newDescription.value,
-    img: newImageUrl.value
+    img: newImageUrl.value,
+    // bcColor: getRandomColor()
   })
-  showModal.value = false
-  newTitle.value = ''
-  newDescription.value = ''
-  newImageUrl.value = ''
-  errorMessage.value = ''
+  showModal.value = false;
+  newTitle.value = '';
+  newDescription.value = '';
+  newImageUrl.value = '';
+  // errorMessage.value = ''
+  isloading.value = false;
 }
 
 const onFileChange = (e) => {
@@ -58,6 +62,10 @@ const onFileChange = (e) => {
   }
 
   reader.readAsDataURL(file)
+}
+
+const delitNote = (index) => {
+  notes.value.splice(index, 1)
 }
 // modal
 </script>
@@ -76,31 +84,13 @@ const onFileChange = (e) => {
               <img src="@/app/img/add-img.svg" alt="Загрузить картинку" />
               <input type="file" />
             </label>
-            <img
-              class="list__img"
-              style="width: 100px; height: 100px"
-              v-if="newImageUrl"
-              :src="newImageUrl"
-              alt="Uploaded Image"
-            />
+            <img class="file__img" v-if="newImageUrl" :src="newImageUrl" alt="Uploaded Image" />
           </div>
           <div class="note-input">
-            <textarea
-              v-model.trim="newTitle"
-              name="note"
-              id="note"
-              cols="32"
-              rows="6"
-              placeholder="Добавьте заголовок..."
-            ></textarea>
-            <textarea
-              v-model.trim="newDescription"
-              name="note"
-              id="note"
-              cols="32"
-              rows="7"
-              placeholder="Добавьте описание..."
-            ></textarea>
+            <textarea v-model.trim="newTitle" name="note" id="note" cols="32" rows="5"
+              placeholder="Добавьте заголовок..."></textarea>
+            <textarea v-model.trim="newDescription" name="note" id="note" cols="32" rows="5"
+              placeholder="Добавьте описание..."></textarea>
             <button class="note-input__button" @click="addNote()">Сохранить</button>
           </div>
         </div>
@@ -128,36 +118,39 @@ const onFileChange = (e) => {
         </div>
         <div class="info-count">
           <p class="info-count__frends">Друзей 10</p>
-          <p class="info-count__list">Листов 3</p>
+          <p class="info-count__list">Желаний {{ notes.length }}</p>
         </div>
       </div>
-      <!-- <div v-if="showModal" class="overlay">
-        <div class="modal">
-          <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
-          <button @click="addNote()">Add note</button>
-          <button @click="showModal = false" class="close">close</button>
-        </div>
-      </div> -->
       <button @click="showModal = true" class="user-create">
         <img src="@/app/img/create-list.svg" alt="Создание листа желания" />
       </button>
     </div>
 
+
     <div class="list">
-      <!-- <div class="list-title">
+
+      <div class="list-title">
         <div class="list-title__line"></div>
         <h2 class="list-title__text">Мой лист</h2>
-      </div> -->
-      <div v-for="note in notes" :key="note.id" class="card">
-        <img class="card__img" :src="note.img" alt="" />
-        <div class="card-text">
+
+      </div>
+      <div class="list-loading" v-if="isloading">
+        <h1 class="list-loading__title">Создайте своё желание</h1>
+        <img class="list-loading__img" src="@/app/img/is-loading.svg" alt="">
+      </div>
+      <div class="cards">
+        <div v-for="note in notes" :key="note.id" class="card">
+          <img class="card__img" :src="note.img" alt="" />
           <p class="card-text__title">{{ note.title }}</p>
           <p class="card-text__subtitle">{{ note.description }}</p>
-        </div>
-        <div class="card-open">
-          <button class="card-open__button">Удалить</button>
+          <div class="card-open">
+            <button type="submit" class="card-open__button" @click="delitNote(index)">Удалить</button>
+          </div>
         </div>
       </div>
+
+
+
     </div>
   </div>
 </template>
@@ -184,7 +177,7 @@ const onFileChange = (e) => {
 
     .modal {
       width: 862px;
-      height: 440px;
+      height: 400px;
       margin: 0 auto;
       background: #fff;
       border-radius: 30px;
@@ -193,12 +186,38 @@ const onFileChange = (e) => {
 
       position: relative;
 
+      &__close {
+        position: absolute;
+        top: 30px;
+        right: 30px;
+      }
+
       .note {
         display: flex;
         // justify-content: space-between;
 
         .file {
           border: 1px solid rgba(247, 110, 80, 0.5);
+          width: 342px;
+          height: 310px;
+          position: relative;
+
+          border-radius: 17px;
+          padding: 34px 68px;
+
+          &__img {
+            width: 270px;
+            height: 290px;
+            border-radius: 10px;
+
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            margin-left: 30px;
+            // right: 0;
+            // bottom: 0;
+          }
 
           input[type='file'] {
             outline: none;
@@ -213,10 +232,23 @@ const onFileChange = (e) => {
           flex-direction: column;
           row-gap: 20px;
 
+          textarea {
+            border: 1px solid rgba(247, 110, 80, 0.5);
+            border-radius: 20px;
+            padding: 8px;
+            outline: #f76e50;
+
+            font-weight: 500;
+            font-size: 14px;
+            margin-left: 20px;
+          }
+
           &__button {
             background-color: #f76e50;
             border-radius: 20px;
             padding: 16px 136px;
+
+            margin-left: 20px;
 
             cursor: pointer;
 
@@ -240,7 +272,7 @@ const onFileChange = (e) => {
       display: flex;
       align-items: center;
 
-      width: 1049px;
+      width: 1100px;
 
       background-color: #fff;
       border-radius: 30px;
@@ -261,6 +293,7 @@ const onFileChange = (e) => {
         display: flex;
         flex-direction: column;
         row-gap: 28px;
+
         &__name {
           font-weight: 500;
           font-size: 22px;
@@ -323,12 +356,36 @@ const onFileChange = (e) => {
 
   .list {
     display: flex;
+    flex-direction: column;
     gap: 20px;
+
+    // height: 600px;
+    padding: 40px;
+
+    background-color: #fff;
+    border-radius: 30px;
+
+    margin-top: 30px;
+
+    &-loading {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      row-gap: 30px;
+
+      &__title {
+        color: rgba(152, 143, 255, 0.928);
+      }
+    }
 
     &-title {
       display: flex;
-      align-items: center;
+      // align-items: center;
       column-gap: 10px;
+
+
+
       &__line {
         background-color: #f76e50;
         box-shadow: 1px 0 23px 0 rgba(247, 110, 80, 0.7);
@@ -337,60 +394,74 @@ const onFileChange = (e) => {
         height: 50px;
       }
 
+      &__text {
+        margin-top: 14px;
+      }
+
     }
 
-    .card {
-      border: 1px solid #f76e50;
-      background-color: #fff;
-      border-radius: 20px;
-      padding: 20px 22px;
-      width: 244px;
-      height: 361px;
+    .cards {
+      display: flex;
+      gap: 20px;
+      flex-wrap: wrap;
 
-      &__img {
-        border-radius: 10px;
-        width: 200px;
-        height: 179px;
-      }
+      margin-top: 10px;
 
-      &-text {
-        display: flex;
-        flex-direction: column;
-        row-gap: 12px;
+      .card {
+        border: 1px solid #f76e50;
+        background-color: #fff;
+        border-radius: 20px;
+        padding: 30px;
+        // width: 240px;
+        // height: 360px;
 
-        margin-top: 20px;
-        &__title {
-          font-weight: 500;
-          font-size: 18px;
-        }
-
-        &__subtitle {
-          font-weight: 500;
-          font-size: 16px;
-          color: #2e2e2e;
-
-          border-bottom: 1px solid #f76e50;
-          padding-bottom: 18px;
-        }
-      }
-
-      &-open {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        margin-top: 18px;
-
-        &__button {
-          font-weight: 500;
-          font-size: 18px;
-          color: #fff;
-
+        &__img {
           border-radius: 10px;
-          padding: 10px 39px;
-          background: #f76e50;
+          width: 170px;
+          height: 179px;
+        }
 
-          cursor: pointer;
+        &-text {
+          display: flex;
+          flex-direction: column;
+          row-gap: 12px;
+
+          margin-top: 20px;
+
+          &__title {
+            font-weight: 500;
+            font-size: 18px;
+          }
+
+          &__subtitle {
+            font-weight: 500;
+            font-size: 16px;
+            color: #2e2e2e;
+
+            border-bottom: 1px solid #f76e50;
+            padding-bottom: 18px;
+          }
+        }
+
+        &-open {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          margin-top: 18px;
+
+
+          &__button {
+            font-weight: 500;
+            font-size: 18px;
+            color: #fff;
+
+            border-radius: 10px;
+            padding: 10px 39px;
+            background: #f76e50;
+
+            cursor: pointer;
+          }
         }
       }
     }
@@ -422,27 +493,6 @@ const onFileChange = (e) => {
         border-radius: 100%;
         color: white;
         font-size: 20px;
-      }
-    }
-
-    &-cards {
-      display: flex;
-      flex-wrap: wrap;
-      .card {
-        width: 255px;
-        height: 255px;
-        padding: 10px;
-        border-radius: 10px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        margin-right: 20px;
-        margin-bottom: 20px;
-
-        &__date {
-          font-size: 12.5px;
-          font-weight: bold;
-        }
       }
     }
   }
