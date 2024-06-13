77779c78e7d7b150c1d906bef7f3a5893afca4a0
Daria Korchunova <123251688+Hartunderblade@users.noreply.github.com>
2024-06-13 18:40:31 +0400

    Add files via upload

--- src/shared/aboutFried/ui.vue
+++ src/shared/aboutFried/ui.vue
@@ -4,6 +4,8 @@ import { useRoute } from 'vue-router';
 
 import friends from '@/friends.json';
 
+const openCard = ref(false);
+
 const cliked = ref(false)
 
 const friend = ref(null);
@@ -27,6 +29,33 @@ const removeFriend = () => {
 <template>
 
     <div class="user">
+        <div v-if="openCard"
+            style="position: fixed; top: 0; left: 0; height: 100%; width: 100%; background: rgba(0, 0, 0, 0.5); z-index: 19; opacity: 70;display: flex;flex-direction: column;justify-content: center;align-items: center;"
+            class="overlay">
+            <div style="width: 862px; height: 400px; margin: 0 auto; background: #fff; border-radius: 30px; padding: 40px; z-index: 20; position: relative;"
+                class="modal">
+                <svg @click="openCard = false" style="cursor: pointer; position: absolute; top: 30px; right: 30px;" width="19" height="18" viewBox="0 0 19 18"
+                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.841797 0L18.8418 18" stroke="#696969" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M0.841797 18L18.8418 0" stroke="#696969" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+                <div style="display: flex; justify-content: space-between;">
+                    <div style="border: 0.86px solid rgba(247, 110, 80, 0.5); border-radius: 17px; padding: 34px 68px;">
+                        <img style="width: 250px; border-radius: 20px; "
+                            src="https://i.pinimg.com/564x/e2/0a/ab/e20aab8ce8d0851a559414b8bb82dbed.jpg" alt="">
+                    </div>
+                    <div class="note-input">
+                        <textarea style="width: 90%; height: 140px; margin-bottom: 18px; font-weight: 400; font-size: 16px; border: 1px solid rgba(247, 110, 80, 0.5); border-radius: 20px; padding: 8px; outline: #f76e50; font-weight: 500; font-size: 14px; margin-left: 20px; pointer-events: none; user-select: none;" name="note" id="note" cols="32" rows="5">На день рождение</textarea>
+                        <textarea style="width: 90%; height: 160px; font-weight: 400; font-size: 16px; border: 1px solid rgba(247, 110, 80, 0.5); border-radius: 20px; padding: 8px; outline: #f76e50; font-weight: 500; font-size: 14px; margin-left: 20px; pointer-events: none; user-select: none;" name="note" id="note" cols="32" rows="5"
+                            placeholder="Добавьте описание...">Подарок на моё 20 летие</textarea>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
         <div class="info" v-if="friend">
             <img class="info__img" :src="friend.avatar" alt="">
             <div class="description">
@@ -68,15 +97,39 @@ const removeFriend = () => {
             </div>
 
             <div class="list-items">
-                <div v-for="item in friend.list" :key="item.id" class="item">
+                <div style="display: flex; flex-direction: column; justify-content: space-between; background-color: #ffeeeb;"
+                    v-for="item in friend.list" :key="item.id" class="item">
                     <img :src="item.img" alt="">
                     <div class="item-text">
                         <h3 class="item-text__title">{{ item.title }}</h3>
                         <p class="item-text__subtitle">{{ item.subtitle }}</p>
                     </div>
-                    <div class="item-use">
-                        <button class="item-use__open">Открыть</button>
-                        <input type="checkbox">
+                    <div style="position: relative; display: flex; align-items: center; justify-content: space-between; border-top: 1px solid #f76e50; padding-top: 16px; margin-top: 16px;"
+                        class="item-use">
+                        <button @click="openCard = true" style="cursor: pointer;"
+                            class="item-use__open">Открыть</button>
+                        <img style="position: absolute; right: 0; top: 20px; width: 32px; height: 32px;"
+                            src="@/app/img/check-card.svg" alt="">
+                    </div>
+                </div>
+                <div style="display: flex; flex-direction: column; justify-content: space-between; "
+                    v-for="item in friend.list" :key="item.id" class="item">
+                    <img :src="item.img" alt="">
+                    <div class="item-text">
+                        <h3 class="item-text__title">{{ item.title }}</h3>
+                        <p class="item-text__subtitle">{{ item.subtitle }}</p>
+                    </div>
+                    <div style="display: flex; align-items: center; justify-content: space-between; border-top: 1px solid #f76e50; padding-top: 16px; margin-top: 16px;"
+                        class="item-use">
+                        <button style="cursor: pointer;" class="item-use__open">Открыть</button>
+                        <svg width="25" height="26" viewBox="0 0 25 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M24.5 13C24.5 19.6274 19.1274 25 12.5 25C5.87258 25 0.5 19.6274 0.5 13C0.5 6.37258 5.87258 1 12.5 1C19.1274 1 24.5 6.37258 24.5 13Z"
+                                fill="#F76E50" fill-opacity="0.1" stroke="#F76E50" />
+                            <path
+                                d="M7.8125 12.9087L11.4749 17.7168C11.9359 18.3219 12.8769 18.2112 13.1848 17.5156L17.8125 7.0625"
+                                stroke="#F76E50" stroke-width="2" stroke-linecap="round" />
+                        </svg>
                     </div>
                 </div>
             </div>
@@ -252,8 +305,12 @@ const removeFriend = () => {
 
                     }
 
-                    input{
-                    }
+                    // input[type="checkbox"]{
+                    //     width: 25px;
+                    //     height: 25px;
+                    //     border-radius: 50%;
+                    //     border: none;
+                    // }
 
                 }
 
